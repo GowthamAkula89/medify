023dc60dd9ac47b9270fd277616d58c371b4751d
Akula Gowtham <gowtham.akula@udtrucks.com>
2024-04-12 13:10:39 +0530

    medify-styles-update

--- src/Components/HeroSection/index.js
+++ src/Components/HeroSection/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./heroSection.css";
+import {Link} from "react-router-dom";
 const HeroSection = () => {
     return(
         <div className="hero-section">
             <div className="hero-subsection">
                 <div className="herosection-text1">Skip the travel! Find Online <span className="herosection-text2">Medical Centers</span></div>
                 <div className="herosection-text3">Connect instantly with a 24x7 specialist or choose to video visit a particular doctor.</div>
-                <button className="btn">Find Centers</button>
+                <Link to="/hospitals" style={{textDecoration:"none"}}><button className="btn">Find Centers</button></Link>
             </div>
             <img src="hero_img.png" alt="hero_img"/>
         </div>
